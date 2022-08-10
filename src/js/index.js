2555beea7f340a558aee1fb0e5b5462b722b1d2b
madara1319 <mateusz.bik@op.pl>
2022-08-10 21:01:35 +0200

    Text editor doesnt really work graphic design will also need updating

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,25 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+const entry = localStorage.getItem('entry');
+let result='';
+if(entry){
+    console.log(`wartość entry ${entry}`)
+    result=entry;
+}
+
+
+
+const entryInput = document.querySelector('form__item');
+entryInput.value=result;
+
+
+
+const button = document.querySelector('save');
+button.addEventListener('click',()=>{
+    localStorage.setItem('entry',entryInput.value);
+})
+
+console.log(entryInput);
