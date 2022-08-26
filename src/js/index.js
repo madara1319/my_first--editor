3e5b460d0757af30e090dc2ce4317c37fc2f1bbf
madara1319 <mateusz.bik@op.pl>
2022-08-26 18:43:57 +0200

    Trynna get the localStorage saving working

--- src/js/index.js
+++ src/js/index.js
@@ -9,23 +9,51 @@ import '../scss/main.scss';
 console.log('HELLO 🚀')
 
 
-const entry = localStorage.getItem('entry');
-let result='';
-if(entry){
-    console.log(`wartość entry ${entry}`)
-    result=entry;
-}
+localStorage.setItem('human','maciek');
+
+console.log(localStorage.getItem('human'));
 
 
 
-const entryInput = document.querySelector('form__item');
-entryInput.value=result;
 
 
 
-const button = document.querySelector('save');
-button.addEventListener('click',()=>{
-    localStorage.setItem('entry',entryInput.value);
-})
 
-console.log(entryInput);
+console.log(JSON.stringify({name:'Mateusz'}));
+
+
+
+localStorage.setItem('nowyKlucz',JSON.stringify({name:'maciek'}));
+
+console.log(localStorage.getItem('nowyKlucz'));
+
+const myResult = localStorage.getItem('nowyKlucz');
+
+console.log(JSON.parse(myResult));
+
+const myNewObject = JSON.parse(myResult);
+
+myNewObject.newProperty='hi';
+
+console.log(myNewObject);
+
+// const entry = localStorage.getItem('entry');
+// let result='';
+// if(entry){
+//     console.log(`wartość entry ${entry}`)
+//     result=entry;
+// }
+
+
+
+// const entryInput = document.querySelector('form__item');
+// entryInput.value=result;
+
+
+
+// const button = document.querySelector('save');
+// button.addEventListener('click',()=>{
+//     localStorage.setItem('entry',entryInput.value);
+// })
+
+// console.log(entryInput);
